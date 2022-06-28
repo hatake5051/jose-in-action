164bae6089910bd7e5931484ac9611067e4aabc2
hatake5051 <hatake5051@gmail.com>
2022-06-28 22:14:36 +0900

    publish directory 作成

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import resolve from 'rollup-plugin-node-resolve';
 export default {
   input: 'build/index.js',
   output: {
-    file: 'bundle.js',
+    file: 'publish/bundle.js',
     format: 'cjs',
   },
   plugins: [resolve(), includePaths({ paths: ['./build'] })],
